b55571bec0b1211b44f51b466112b55f00fc3b21
“BennettKnight” <bennett.dell.knight@gmail.com>
2024-11-09 12:45:00 -0300

    Mobile Updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 // App.js
 import React from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
 import ToneComparisonApp from './components/ToneComparisonApp';
 import './reset.css';
 
 function App() {
   return (
-    <div className="min-h-screen w-full" style={{ background: '#090919' }}>
-      <div className="container mx-auto px-4 py-8">
-        <ToneComparisonApp />
+    <ErrorBoundary>
+      <div className="min-h-screen w-full" style={{ background: '#090919' }}>
+        <div className="container mx-auto px-4 py-8">
+          <ToneComparisonApp />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
